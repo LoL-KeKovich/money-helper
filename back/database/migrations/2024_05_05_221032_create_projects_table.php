d55a49830d74b9a27aadda02754072f28630aff0
LoL-KeKovich <120039879+LoL-KeKovich@users.noreply.github.com>
2024-05-06 16:30:56 +0300

    initialised project with basic functionality

---
+++ back/database/migrations/2024_05_05_221032_create_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->bigInteger('current_sum')->nullable();
+            $table->tinyInteger('current_status')->nullable();
+            $table->bigInteger('needed_sum')->nullable();
+            $table->string('author');
+            $table->foreign('author')->references('email')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
